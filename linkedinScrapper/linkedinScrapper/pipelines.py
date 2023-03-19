af52d6b6c0157797ba9dfa84049f22b4f98461c2
Amine <amine.mekki@mines-ales.org>
2023-03-19 20:53:41 +0100

    Adding some changes to the code, bugs to fix in other time

--- linkedinScrapper/linkedinScrapper/pipelines.py
+++ linkedinScrapper/linkedinScrapper/pipelines.py
@@ -35,31 +35,31 @@ class LinkedinscrapperPipeline:
 
     def process_item(self, item, spider):
 
-        # ## Check to see if text is already in database 
-        # self.cur.execute("select * from Linkedin_jobs where content = %s", (item['job_detail_url'],))
-        # result = self.cur.fetchone()
+        ## Check to see if text is already in database 
+        self.cur.execute("select * from Linkedin_jobs where job_detail_url = %s", (item['job_detail_url'],))
+        result = self.cur.fetchone()
 
-        # ## If it is in DB, create log message
-        # if result:
-        #     spider.logger.warn("Item already in database: %s" % item['job_detail_url'])
+        ## If it is in DB, create log message
+        if result:
+            spider.logger.warn("Item already in database: %s" % item['job_detail_url'])
 
 
-        # ## If text isn't in the DB, insert data
-        # else:
+        ## If text isn't in the DB, insert data
+        else:
 
-        ## Define insert statement
-        self.cur.execute(""" insert into Linkedin_jobs(job_title, job_detail_url, job_listed, company_name, company_link, company_location) values (%s,%s,%s,%s,%s,%s)""", (
-        str(item["job_title"]),
-        str(item["job_detail_url"]),
-        str(item["job_listed"]),
-        str(item["company_name"]),
-        str(item["company_link"]),
-        str(item["company_location"])
-        ))
+            # Define insert statement
+            self.cur.execute(""" insert into Linkedin_jobs(job_title, job_detail_url, job_listed, company_name, company_link, company_location) values (%s,%s,%s,%s,%s,%s)""", (
+            str(item["job_title"]),
+            str(item["job_detail_url"]),
+            str(item["job_listed"]),
+            str(item["company_name"]),
+            str(item["company_link"]),
+            str(item["company_location"])
+            ))
 
-        ## Execute insert of data into database
-        self.connection.commit()
-        return item
+            ## Execute insert of data into database
+            self.connection.commit()
+            return item 
     
     def close_spider(self, spider):
 
--- linkedinScrapper/linkedinScrapper/spiders/linkedin_jobs.py
+++ linkedinScrapper/linkedinScrapper/spiders/linkedin_jobs.py
@@ -1,6 +1,6 @@
 import scrapy
-
-
+import json
+import csv
 from linkedinScrapper.items import LinkedinscrapperItem
 
 
@@ -13,7 +13,24 @@ class LinkedJobsSpider(scrapy.Spider):
         first_url = self.api_url + str(first_job_on_page)
         yield scrapy.Request(url=first_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
 
+    def save_record_to_file(self, record, filename):
+        filename += ".json"
+        with open(filename, 'a') as f:
+            json.dump(record, f)
+            f.write(',\n')
+            
+    def to_csv(filename):
+        with open(filename + '.json', 'r') as json_file:
+            data = json.load(json_file)
 
+        with open(filename+'.csv', 'w', newline='') as csv_file:
+            writer = csv.writer(csv_file)
+
+            writer.writerow(data[0].keys())
+
+            for row in data:
+                writer.writerow(row.values())
+            
     def parse_job(self, response):
         first_job_on_page = response.meta['first_job_on_page']
 
@@ -24,7 +41,6 @@ class LinkedJobsSpider(scrapy.Spider):
         print("******* Num Jobs Returned *******")
         print(num_jobs_returned)
         print('*****')
-        
         for job in jobs:
             
             job_item['job_title'] = job.css("h3.base-search-card__title::text").get(default='not-found').strip()
@@ -34,10 +50,12 @@ class LinkedJobsSpider(scrapy.Spider):
             job_item['company_name'] = job.css('h4 a.hidden-nested-link::text').get(default='not-found').strip()
             job_item['company_link'] = job.css('h4 a.hidden-nested-link::attr(href)').get(default='not-found')
             job_item['company_location'] = job.css('span.job-search-card__location::text').get(default='not-found').strip()
-            yield job_item
-        
+            self.save_record_to_file(job_item, "Linkedin_job_offers")
 
+        
         if num_jobs_returned > 0:
             first_job_on_page = int(first_job_on_page) + 25
             next_url = self.api_url + str(first_job_on_page)
             yield scrapy.Request(url=next_url, callback=self.parse_job, meta={'first_job_on_page': first_job_on_page})
+            
+        self.to_csv("Linkedin_job_offers")
